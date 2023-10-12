5a0a5168029aef75543f342d860942a11c2376c6
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-10-12 16:58:56 -0700

    add createGridDisplay function with cell style

--- src/index.js
+++ src/index.js
@@ -17,3 +17,18 @@ element.innerHTML = `
   </div>
 `;
 document.body.appendChild(element);
+
+function createGridDisplay(rows, cols) {
+  const boards = document.querySelectorAll(".board");
+  boards.forEach((board) => {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        board.appendChild(cell);
+      }
+    }
+  });
+}
+
+createGridDisplay(10, 10);
